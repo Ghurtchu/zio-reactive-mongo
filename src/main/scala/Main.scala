aaa4e6ae1cf8432e53b4f21b659d4ffe40e8ce0b
nghurtch <nghurtch@broadinstitute.com>
2022-08-31 12:13:27 +0400

    apply ZLayers, initialize DB, create Dao implementation, read users and print them

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,36 +4,69 @@ import org.mongodb.scala.bson.ObjectId
 import zio.*
 import org.mongodb.scala.model.Filters.*
 
+import scala.concurrent.Future
+
+final case class User(id: ObjectId, name: String, email: String, password: String)
+
+trait Database[A] {
+  def get: A
+}
+
+final case class MongoDatabaseLive() extends Database[MongoDatabase] {
+
+  override lazy val get: MongoDatabase = {
+    val client: MongoClient = MongoClient("mongodb://localhost:27018")
+
+    client.getDatabase("userdb")
+  }
+
+}
+
+object MongoDatabaseLive {
+
+  def layer: ULayer[Database[MongoDatabase]] = ZLayer.fromFunction(MongoDatabaseLive.apply _)
+
+}
+
+trait Dao[A] {
+  def getAll: IO[Throwable, Seq[A]]
+}
+
+final case class UserDao(database: Database[MongoDatabase]) extends Dao[User] {
+
+  override def getAll: IO[Throwable, Seq[User]] = for {
+    userDocuments <- ZIO.fromFuture(implicit ec => database.get.getCollection("user").find.toFuture())
+    users         <- ZIO.succeed {
+      userDocuments
+        .map(_.toMap)
+        .map { userMap =>
+          User(
+            id = userMap("_id").asObjectId().getValue,
+            name = userMap("name").asString().getValue,
+            email = userMap("email").asString().getValue,
+            password = userMap("password").asString().getValue
+          )
+        }
+    }
+  } yield users
+
+}
+
+object UserDao {
+
+  def layer: URLayer[Database[MongoDatabase], Dao[User]] = ZLayer.fromFunction(UserDao.apply _)
+
+}
+
 
 object Main extends ZIOAppDefault {
 
-  val database: UIO[MongoDatabase] = ZIO.succeed {
-    val uri: String = "mongodb://localhost:27018"
-    val client: MongoClient = MongoClient(uri)
+  val getAllUsers: ZIO[Dao[User], Throwable, Unit] =
+    for {
+      db    <- ZIO.service[Dao[User]]
+      users <- db.getAll.tap(Console.printLine(_))
+    } yield ()
 
-    client.getDatabase("notesdb")
-  }
-
-  final case class User(id: ObjectId, name: String, email: String, password: String)
-
-  val program: Task[Unit] = for {
-    db          <- database
-    users       <- ZIO.fromFuture(implicit ex => db.getCollection("user").find().toFuture())
-    listOfUsers <- ZIO.succeed {
-      users
-        .map(_.toMap)
-        .map { userMap =>
-        User(
-          id       = userMap("_id").asObjectId().getValue,
-          name     = userMap("name").asString().getValue,
-          email    = userMap("email").asString().getValue,
-          password = userMap("password").asString().getValue
-        )
-      }
-    }
-    _           <- ZIO.succeed(println(listOfUsers))
-  } yield ()
-
-  override def run = program
+  override def run: Task[Unit] = getAllUsers.provide(UserDao.layer, MongoDatabaseLive.layer)
 
 }
