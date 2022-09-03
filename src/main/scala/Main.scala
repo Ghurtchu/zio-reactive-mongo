b051c84f2f94df2e541cb06793cd1fe1cc5ce4ee
nghurtch <nghurtch@broadinstitute.com>
2022-09-03 14:55:26 +0400

    use lazy vals, extract method for parsing doc to user

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,27 +8,19 @@ import org.mongodb.scala.model.Filters.*
 import scala.concurrent.Future
 
 final case class User(id: ObjectId, name: String, email: String, password: String)
-
 final case class DBConfig(port: String, name: String)
 
-final case class MongoDatabaseBuilder(dbConfig: DBConfig) {
-
-  lazy val build: MongoDatabase = {
-    val client: MongoClient = MongoClient(dbConfig.port)
-    client.getDatabase(dbConfig.name)
-  }
-
+object MongoDatabaseBuilder {
+  lazy final val build: UIO[MongoDatabase] = (for {
+    port          <- System.envOrElse("MONGO_PORT", "mongodb://localhost:27018")
+    name          <- System.envOrElse("MONGO_DB_NAME", "notesdb")
+    client        <- ZIO.attempt(MongoClient(port))
+    mongoDatabase <- ZIO.attempt(client.getDatabase(name))
+  } yield mongoDatabase).orDie
 }
 
 object MongoDatabaseProvider {
-
-  val get: MongoDatabase = MongoDatabaseBuilder(
-    DBConfig(
-      port ="mongodb://localhost:27018",
-      name = "notesdb"
-    )
-  ).build
-
+  lazy final val get: UIO[MongoDatabase] = MongoDatabaseBuilder.build
 }
 
 trait Dao[A] {
@@ -37,30 +29,26 @@ trait Dao[A] {
 
 final case class UserDao() extends Dao[User] {
 
-  val mongo: MongoDatabase = MongoDatabaseProvider.get
+  val mongo: UIO[MongoDatabase] = MongoDatabaseProvider.get
 
   override def getAll: IO[Throwable, Seq[User]] = for {
-    userDocuments <- ZIO.fromFuture(implicit ec => mongo.getCollection("user").find.toFuture())
-    users         <- ZIO.succeed {
-      userDocuments
-        .map(_.toMap)
-        .map { userMap =>
-          User(
-            id = userMap("_id").asObjectId().getValue,
-            name = userMap("name").asString().getValue,
-            email = userMap("email").asString().getValue,
-            password = userMap("password").asString().getValue
-          )
-        }
-    }
+    db            <- mongo
+    userDocuments <- ZIO.fromFuture(implicit ec => db.getCollection("user").find.toFuture())
+    users         <- ZIO.succeed(userDocuments.map(parseDocumentToUser))
   } yield users
+
+  private def parseDocumentToUser(doc: Document): User =
+    User(
+      id       = doc("_id").asObjectId().getValue,
+      name     = doc("name").asString().getValue,
+      email    = doc("email").asString().getValue,
+      password = doc("password").asString().getValue
+    )
 
 }
 
 object UserDao {
-
   def layer: ULayer[Dao[User]] = ZLayer.succeed(UserDao())
-
 }
 
 final case class UserDaoTest() extends Dao[User] {
@@ -68,9 +56,7 @@ final case class UserDaoTest() extends Dao[User] {
 }
 
 object UserDaoTest {
-
   def layer: ULayer[Dao[User]] = ZLayer.succeed(UserDaoTest())
-
 }
 
 
